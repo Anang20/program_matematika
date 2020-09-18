3acfe19cccb883be2a942602a98054b2d48d0fb5
miftah <70564481+miftachul10@users.noreply.github.com>
2020-09-17 19:27:46 -0700

    pertambahan

---
+++ pertambahan.php
@@ -0,0 +1,40 @@
+<?php
+
+$keluar=true;
+
+while($keluar)
+{
+    // Banner Aplikasi
+
+    echo "Aplikasi Perhitungan Pemjumlahan!!!\n";
+
+    // Terima input dari user
+
+    $angka_pertama=readline("Masukkan Angka Pertama \t :");
+    $angka_kedua=readline("Masukkan Angka Kedua :");
+
+
+    // Validasi input 
+
+
+    if(!is_numeric($angka_pertama) || !is_numeric($angka_kedua)){
+        echo "Maaf anda hanya bisa menginput angka !!!\n";
+        continue;
+    }
+
+    // Tampilkan hasil perhitungan
+
+    echo "hasil dari $angka_pertama + $angka_kedua adalah : ".($angka_pertama+$angka_kedua)."\n";
+
+    // Cek logika user untuk mengulangi
+
+    $ulangi=readline("Ulangi Lagi (yes/no) :");
+
+    if($ulangi=="no" || $ulangi!=="yes")
+    {
+        $keluar=false;
+    }
+    
+
+}
+?>
