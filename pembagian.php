b4f02f239b7841cfaba592152bca48644fce4407
Khoirudinn <70564983+Dynn01@users.noreply.github.com>
2020-09-18 08:25:43 +0700

    pembagian

---
+++ pembagian.php
@@ -0,0 +1,30 @@
+<?php 
+
+$keluar=true;
+
+while($keluar)
+{
+    // terima input dari user
+
+    $angka_pertama=readline("Masukkan Angka Pertama :");
+    $angka_kedua=readline("Masukkan Angka Kedua :");
+
+    // validasi input angka 
+
+    if(!is_numeric($angka_pertama) || !is_numeric($angka_kedua)){
+        echo "Maaf input hanya bisa angka !! \n";
+    continue;
+    }
+
+     // Tampilkan hasil perhitungan
+
+     echo "hasil dari $angka_pertama dan $angka_kedua adalah :".($angka_pertama/$angka_kedua)."\n";
+
+     // cek logika user untuk mengulangi
+
+    $ulangi=readline("Ulangi Lagi (yes/no) :");
+
+    if($ulangi=="no" || $ulangi!=="yes"){
+       $keluar=false;
+    }
+}
